6cd88b6fccbe55a99054130d11908e6b942a26ce
noctuelles <plouvel@student.42.fr>
2025-03-14 18:54:37 +0100

    feat: added libc

---
+++ libc/src/stdlib/abort.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+__attribute__((__noreturn__)) void abort(void)
+{
+#if defined(__is_libk)
+    // TODO: Add proper kernel panic.
+#else
+    // TODO: Abnormally terminate the process as if by SIGABRT.
+#endif
+    while (1)
+    {
+    }
+    __builtin_unreachable();
+}
