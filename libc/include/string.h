e1b436cca1818bab1387d2edbbdf6f0e4de14ea4
noctuelles <plouvel@student.42.fr>
2025-04-01 16:17:38 +0200

    feat(libc): added strcmp

--- libc/include/string.h
+++ libc/include/string.h
@@ -15,6 +15,7 @@ extern "C"
     void *memcpy(void *__restrict, const void *__restrict, size_t);
     void *memmove(void *, const void *, size_t);
     void *memset(void *, int, size_t);
+    int strcmp(const char *, const char *);
     size_t strlen(const char *);
     char *itoa_base(int64_t n, const char *base);
     char *utoa_base(uint64_t n, const char *base);
